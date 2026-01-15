fb8aa0c684468892844bb61eeefaf328265cd81b
emercency-meissen <robotikga@gmail.com>
2026-01-16 00:45:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+// ALLES aus Root laden
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 let waiting = null;
 
@@ -37,9 +43,8 @@ io.on("connection", socket => {
   socket.on("skip", () => {
     if (socket.partner) {
       io.to(socket.partner).emit("end");
-      socket.partner = null;
     }
-    socket.emit("end");
+    socket.partner = null;
     socket.emit("waiting");
   });
 
