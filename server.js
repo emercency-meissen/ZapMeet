511a14dfc4967cb4a57db19addc3fd411b9ce1e7
emercency-meissen <robotikga@gmail.com>
2026-01-16 15:36:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,43 +8,27 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(__dirname));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/", (_, res) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
 let waiting = null;
 
 io.on("connection", socket => {
-  console.log("CONNECTED", socket.id);
-
   socket.on("start", () => {
     if (waiting && waiting.id !== socket.id) {
-      const partner = waiting;
+      socket.emit("matched", { id: waiting.id, init: true });
+      waiting.emit("matched", { id: socket.id, init: false });
       waiting = null;
-
-      socket.partner = partner.id;
-      partner.partner = socket.id;
-
-      socket.emit("match", { partnerId: partner.id, initiator: true });
-      partner.emit("match", { partnerId: socket.id, initiator: false });
     } else {
       waiting = socket;
       socket.emit("waiting");
     }
   });
 
-  socket.on("signal", ({ to, data }) => {
-    io.to(to).emit("signal", { from: socket.id, data });
-  });
-
-  socket.on("disconnect", () => {
-    if (waiting?.id === socket.id) waiting = null;
-    if (socket.partner) {
-      io.to(socket.partner).emit("leave");
-    }
-  });
+  socket.on("signal", d =>
+    io.to(d.to).emit("signal", d)
+  );
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("ZapMeet läuft");
-});
+server.listen(process.env.PORT || 3000);
